e4a9c9cca412b3f0e605a18d9e2c7ac655512427
JIYeon.L <a01092857503@gmail.com>
2024-08-07 13:24:16 +0900

    [feat] FromSeconds / DelayType.UnscaledDeltaTime
    특정시간 딜레이 후 함수호출 / 타임스케일 0일때도 동작

--- Assets/UniTaskSecond.cs
+++ Assets/UniTaskSecond.cs
@@ -1,18 +1,23 @@
+using Cysharp.Threading.Tasks;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UniTaskSecond : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //TimeScale이 0일때도 동작
+    //DelayType.UnscaledDeltaTime
+    private async UniTaskVoid WaitSecond()
     {
-        
+        await UniTask.Delay(TimeSpan.FromSeconds(1f), DelayType.UnscaledDeltaTime));
+        Debug.Log("1");
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Time.timeScale = 0f;
+        WaitSecond().Forget();
     }
+
 }
